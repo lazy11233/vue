ae57001464f8b8de31b0c1cbf44491923de4055a
lazy11233 <1257625996@qq.com>
2018-08-24 18:15:15 +0800

    vue project

---
+++ vue-cli/vueapp/src/components/HelloEarth.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <h3>{{$route.params.earthmsg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloEarth',
+  data () {
+    return {
+      msg: 'HelloEarth'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
---
+++ vue-cli/vueapp/src/components/list.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <router-link :to="{name:'HelloWorld',params:{worldmsg:'你好world'}}">Hello World</router-link>
+            </li>
+            <li>
+                <router-link :to="{name:'HelloEarth',params:{earthmsg:'你好earth'}}">Hello Earth</router-link>
+            </li>
+        </ul>
+    </div>    
+</template>
+<script>
+export default {
+  name: "list"
+};
+</script>
+<style scoped>
+li {
+  padding: 3px;
+  list-style: none;
+  font-size: 30px;
+}
+</style>
+
